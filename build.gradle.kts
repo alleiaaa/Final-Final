6a50dc5e5698f1d61479743bf62d87e8627da27b
alleiaaa <alleiatrisha.gula@gmail.com>
2025-02-25 02:49:58 +0800

    commit and push

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,12 +32,16 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.fragment:fragment:1.6.1")
+    implementation ("androidx.viewpager:viewpager:1.0.0")
+    implementation ("com.google.android.material:material:1.9.0")
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
 }
---
+++ src/main/java/com/example/seller_tapnbite_final/fragments/InventoryMain.java
@@ -0,0 +1,26 @@
+package com.example.seller_tapnbite_final.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import com.example.seller_tapnbite_final.R;
+
+public class InventoryMain extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_inventory_main, container, false);
+
+        return view;
+    }
+}
